0e0e952cb6c634676a0adfd4675b2ef6e99e0066
Shreya Sharma <sharma.shreya3@northeastern.edu>
2022-11-26 17:12:54 -0500

    added Login UI and Register UI with code setup of Layout architecture

--- credcheck-frontend/components/header/header.tsx
+++ credcheck-frontend/components/header/header.tsx
@@ -1,10 +1,10 @@
 
-export default function Header () {
+export default function Header() {
     return(
        <nav className="d-flex navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
        <a href="index.html" className="navbar-brand d-flex align-items-center">
            <h2 className="m-0 text-primary"><img className="img-fluid me-2" src="img/icon-1.png" alt=""
-                   style={{width: 45+ 'px'}}/>CryptoCoin</h2>
+                   style={{width: 45+ 'px'}}/>CredCheck</h2>
        </a>
        <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
            <span className="navbar-toggler-icon"></span>
---
+++ credcheck-frontend/components/layout/layout.tsx
@@ -0,0 +1,12 @@
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+export default function Layout({children}:{children: any}): JSX.Element {
+    return (
+        <>
+            <Header/>
+            {children}
+            <Footer/>
+        </>
+    )
+}
