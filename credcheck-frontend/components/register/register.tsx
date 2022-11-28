b63ac3b29ffb8b28c9a882621774c795d1502467
Purbasha Pan <pan.pu@northeastern.edu>
2022-11-28 12:47:35 -0500

    Changed hyperlink in registration

--- credcheck-frontend/components/register/register.tsx
+++ credcheck-frontend/components/register/register.tsx
@@ -1,35 +1,37 @@
+import Link from "next/link";
+
 export default function RegisterForm()  {
     return (
         <form>
         <div className="row g-3">
             <div className="col-md-6">
                 <div className="form-floating">
-                    <input type="text" className="form-control" id="name" placeholder="Your Name"/>
-                    <label htmlFor="name">First Name</label>
+                    <input type="text" className="form-control" id="FirstName" placeholder="First Name"/>
+                    <label htmlFor="Fname">First Name</label>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="form-floating">
-                    <input type="email" className="form-control" id="email" placeholder="Your Email"/>
-                    <label htmlFor="email">Last Name</label>
+                    <input type="email" className="form-control" id="LastName" placeholder="Last Name"/>
+                    <label htmlFor="Lname">Last Name</label>
                 </div>
             </div>
             <div className="col-12">
                 <div className="form-floating">
-                    <input type="text" className="form-control" id="subject" placeholder="Subject"/>
-                    <label htmlFor="subject">Email</label>
+                    <input type="text" className="form-control" id="Email" placeholder="Email"/>
+                    <label htmlFor="email">Email</label>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="form-floating">
-                    <input type="text" className="form-control" id="name" placeholder="Your Name"/>
-                    <label htmlFor="name">Password</label>
+                    <input type="text" className="form-control" id="Password" placeholder="Password"/>
+                    <label htmlFor="password">Password</label>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="form-floating">
-                    <input type="text" className="form-control" id="name" placeholder="Your Name"/>
-                    <label htmlFor="name">Confirm Password</label>
+                    <input type="text" className="form-control" id="ConfirmPassword" placeholder="ConfirmPassword"/>
+                    <label htmlFor="confirm-pass">Confirm Password</label>
                 </div>
             </div>
             {/* <div className="col-12">
@@ -40,7 +42,9 @@ export default function RegisterForm()  {
             </div> */}
             <div className="col-12">
                 <button className="btn btn-primary py-3 px-4" type="submit">Register</button>
-                <a href="http://localhost:3000/login">Login</a>
+                <Link href="/login">
+                Login
+                </Link>
             </div>
         </div>
     </form>
