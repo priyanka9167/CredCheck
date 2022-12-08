2af475a1f4b709561952cbbe50e8a32ec1a1335e
Purbasha Pan <pan.pu@northeastern.edu>
2022-12-07 21:54:21 -0500

    added expenditure logic

--- credcheck-frontend/services/expenditure.ts
+++ credcheck-frontend/services/expenditure.ts
@@ -9,3 +9,12 @@ export const fetchAmountDue = async (cardId: String): Promise<any> => {
         return err;
     }
 }
+
+export const fetchExpenditureDetails = async(cardId: String): Promise<any> => {
+    try {
+        const res = await api.get(`${API_CONSTANTS.BASE_URL}${API_CONSTANTS.EXPENDITURE}${cardId}`);
+        return res;
+    } catch (err) {
+        return err;
+    }
+}
